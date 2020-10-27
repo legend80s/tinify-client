e7a9f815281120adeec5b2405102d999b62da25f
chuanzong.lcz@alibaba-inc.com <chuanzong.lcz@alibaba-inc.com>
2020-10-28 00:28:12 +0800

    test: add jest

---
+++ test/lite-fs.test.js
@@ -0,0 +1,27 @@
+const { isDirectory } = require('../src/utils/lite-fs');
+
+describe('lite-fs', () => {
+  it('isDirectory false', () => {
+    const input = './alipay-logo.png';
+    const actual = isDirectory(input);
+    const expected = false;
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('isDirectory true', () => {
+    const input = './';
+    const actual = isDirectory(input);
+    const expected = true;
+
+    expect(actual).toEqual(expected);
+  });
+
+  it('isDirectory true', () => {
+    const input = './alipay-logo';
+    const actual = isDirectory(input);
+    const expected = true;
+
+    expect(actual).toEqual(expected);
+  });
+});
