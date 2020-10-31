85ec72be732dad1af59a85096a16223f9eaaba17
chuanzong.lcz@alibaba-inc.com <chuanzong.lcz@alibaba-inc.com>
2020-11-01 01:23:10 +0800

    feat: log base64 str on file size < 1024B

--- src/commands/executeBase64Command.js
+++ src/commands/executeBase64Command.js
@@ -3,6 +3,7 @@ const { copyBase64 } = require('../utils/copyBase64');
 const { getFileSize } = require('@legend80s/image-to-base64');
 const { base64Usage } = require('../constants');
 const { decorated } = require('../utils/decorated-console');
+const { toReadableSize } = require('../utils/number');
 
 /**
  * @param {Map<string, any>} options
@@ -27,11 +28,11 @@ exports.executeBase64Command = async options => {
         throw new Error('invalid image, size is ' + size);
       }
 
-      decorated.info('image size:', size, 'Bytes');
+      decorated.info('image size:', toReadableSize(size));
 
       const base64 = await imageToBase64(img);
 
-      copyBase64(base64, { verbose });
+      copyBase64(base64, { verbose: verbose || size < 1024 });
 
       decorated.success('base64 has been copied to your clipboard.');
       decorated.timeEnd('image to base64 costs:');
