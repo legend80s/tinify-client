835353417d1437ec3fd5114d1d7a5e148b8d7d39
chuanzong.lcz@alibaba-inc.com <chuanzong.lcz@alibaba-inc.com>
2020-11-06 22:07:33 +0800

    fix: divided by 0 reduce NaN when computing off percentage

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,7 +1,7 @@
 export function getPercentageOff(before: number, after: number): string {
   const diff = before - after;
 
-  return (diff / before * 100).toFixed(2) + '%';
+  return (before !== 0 ? (diff / before * 100).toFixed(2) : '0') + '%';
 }
 
 export function toReadableSize(sizeInByte: number) {
